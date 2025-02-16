739992373d337a44bbac70019f97f8bbf4bf060d
Likhitha A L <vijethsm@Mac.lan>
2025-02-17 01:08:24 +0530

    prime numbers

--- Logical_Problems/prime_number.c
+++ Logical_Problems/prime_number.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+bool isPrime(int num);
 
 int main(){
 
-
-
+    for(int i=0;i < 100 ; i++){
+        if(isPrime(i))
+            printf(i);
+    }
 
 return 0;
+}
+
+bool isPrime(int num){
+    if(num <= 1)
+        return false;
+
+    for(int index=2; index * index <= num; index++){
+        if(num % index == 0)
+            return false;
+    }
+    return true;
+
 }
