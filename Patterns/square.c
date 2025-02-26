7bbc2e0ef744953c58666e6ac0846a83526c4122
LIKHITHA <likhithaal2001@gmail.com>
2025-02-26 22:48:33 +0530

    square printing

--- Patterns/square.c
+++ Patterns/square.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-#include <stdint.h>
 
+void print_pattern_square(int length);
 
-int main() {
+int main()
+{
+    int len = 0;
+    printf("enter the length\n");
+    scanf("%d",&len);
+    print_pattern_square(len);
 
+    return 0;
+}
 
-  for(int i=0;i<10;i++)
-  {
-    //added
-      
-    
-  }
-  return 0;
+void print_pattern_square(int length)
+{
+    for(int i=0;i<length;i++)
+    {
+        for(int j=0;j<length;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
