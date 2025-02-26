330a4793d8bb71896edef6f3c9ca967566efb3b7
LIKHITHA <likhithaal2001@gmail.com>
2025-02-26 20:54:32 +0530

    added sorting check flag

--- Logical_Problems/bubble_sort.c
+++ Logical_Problems/bubble_sort.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-void sorting(int *arr,int n);
+int sorting(int *arr,int n);
 void printArr(int *arr,int n);
 
 int main(){
     
     int arr[5] = {22, 154, 6587,0, 654};
     
-    sorting(arr,5);
-    printArr(arr,5);
+    if(sorting(arr,5))
+        printArr(arr,5);
+    else
+        printf("Array is already sorted\n");
     
     return 0;
 }
 
-void sorting(int *arr,int n){
-    int temp = 0;
+int sorting(int *arr,int n){
+    int temp = 0,sort_check = 0;
     for(int i=0;i< n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j] > arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                sort_check = 1;
             }
         }
     }
+    return sort_check;
 }
 
 
