2e6e4e6c33b0008d362a0c9b2d1bf5546b172328
LIKHITHA <likhithaal2001@gmail.com>
2025-02-27 13:57:41 +0530

    strong number

---
+++ Logical_Problems/numbers/strong_num.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int factorial(int n);
+int is_strong(int m);
+
+int main()
+{
+    //strong num: sum of factorial of digit is same as the number
+    int num=0,res=0;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    if(is_strong(num))
+        printf("strong number\n");
+    else 
+        printf("Not a strong number\n");
+    
+    return 0;
+}
+
+
+int is_strong(int m)
+{
+    int origin_num=0,rem=0,res=0;
+    origin_num = m;
+    while(m > 0)
+    {
+        rem = m % 10;
+        res += factorial(rem);
+        m = m /10;
+    }
+    return res == origin_num;
+    
+}
+
+int factorial(int n){
+    int fact = 1;
+    if (n == 0){
+        fact = 1;
+        return fact;
+    }
+    for(int j=n; j>1;j--)
+    {
+        fact = fact * j;
+    }
+    return fact;
+}
