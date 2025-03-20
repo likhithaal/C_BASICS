a8fc48463f191414e2fc0b1d3eab34058b69127f
Likhitha A L <likhithaal@Mac.lan>
2025-03-20 20:28:30 +0530

    clear nth bit

---
+++ Bitwise_operators/clear_n_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int clear_bit(int n,int p);
+
+int main(){
+    int num = 0,pos = 0;
+    printf("Enter the number: \n");
+    scanf("%d",&num);
+    printf("Enter the position to clear \n");
+    scanf("%d",&pos);
+    num = clear_bit(num,pos);
+    printf("Number after toggle: %d\n",num);
+    return 0;
+}
+
+int clear_bit(int n,int p){
+    return n & ~(1 << p);      //AND (&) operator with ~1 to clear
+}
