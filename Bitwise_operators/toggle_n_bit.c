36a6a73343449b45fdfa0bc79c4fe6ad1de66aea
Likhitha A L <likhithaal@Mac.lan>
2025-03-20 20:14:40 +0530

    toggle nth bit

---
+++ Bitwise_operators/toggle_n_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int toggle_bit(int n,int p);
+
+int main(){
+    int num = 0,pos = 0;
+    printf("Enter the number: \n");
+    scanf("%d",&num);
+    printf("Enter the position to toggle: \n");
+    scanf("%d",&pos);
+    num = toggle_bit(num,pos);
+    printf("Number after toggle: %d\n",num);
+    return 0;
+}
+
+int toggle_bit(int n,int p){
+    return n ^ (1 << p);
+}
